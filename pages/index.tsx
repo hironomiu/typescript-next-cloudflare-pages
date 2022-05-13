cd48af95fe130e03acf40ae73c99f083b45df9f3
hironomiu <hironomiu@gmail.com>
2022-05-13 15:55:58 +0900

    commited

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,21 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 const Home = ({ blogs }: any) => {
   // return (
-  //   <div>
-  //     {blogs.map((blog: any) => (
-  //       <div key={blog.id}>
-  //         <Link href={`/blogs/${blog.id}`}>
-  //           <a>{blog.title}</a>
-  //         </Link>
-  //       </div>
-  //     ))}
-  //   </div>
+
   // )
-  return <Layout>hoge</Layout>
+  return (
+    <Layout>
+      <div>
+        {blogs.map((blog: any) => (
+          <div key={blog.id}>
+            <Link href={`/blogs/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
 }
 
 export const getStaticProps = async () => {
