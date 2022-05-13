2d30de25ee694cef005b4b3c54da669cfb803aca
hironomiu <hironomiu@gmail.com>
2022-05-13 18:15:40 +0900

    commited

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,20 @@ import Layout from '../components/Layout'
 const Home = ({ blogs }: any) => {
   return (
     <Layout>
-      <main className="flex m-4">
+      <main className="flex flex-wrap">
         {blogs.map((blog: any) => (
           <div
-            className="m-2 p-2 border-2 hover:shadow-2xl hover:bg-gray-300 rounded"
+            className='className="flex flex-col m-4 w-full md:w-1/3 flex-grow flex-shrink"'
             key={blog.id}
           >
-            <Link href={`/blogs/${blog.id}`}>
-              <a>
-                <img
-                  src={blog.eyecatch.url}
-                  alt="eycatch"
-                  height="300"
-                  width="400"
-                />
-                <span className="text-xl">{blog.title}</span>
-              </a>
-            </Link>
+            <div className="flex-1 p-2 border-2 hover:shadow-2xl hover:bg-gray-300 rounded">
+              <Link href={`/blogs/${blog.id}`}>
+                <a>
+                  <img src={blog.eyecatch.url} alt="eycatch" />
+                  <span className="text-xl">{blog.title}</span>
+                </a>
+              </Link>
+            </div>
           </div>
         ))}
       </main>
